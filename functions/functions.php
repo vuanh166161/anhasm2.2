2eec11b8a68fa8d64bcc394078494d1ace78dc11
vuanh166161 <anhnvgcc200163@fpt.edu.vn>
2022-10-24 13:43:11 +0700

    test

--- functions/functions.php
+++ functions/functions.php
@@ -2,7 +2,7 @@
 
 <?php 
 // $con = mysqli_connect("localhost","root","","ecommerce_cms_tutorial");
-$Connect = pg_connect("postgres://cmvqjnqqlmumse:1dc0a0f9facb2b6081d619eeec13ce6a759ddee3efcc085f309ab8a04814caa4@ec2-54-82-205-3.compute-1.amazonaws.com:5432/d7h1ne0bj2a348
+$con = pg_connect("postgres://cmvqjnqqlmumse:1dc0a0f9facb2b6081d619eeec13ce6a759ddee3efcc085f309ab8a04814caa4@ec2-54-82-205-3.compute-1.amazonaws.com:5432/d7h1ne0bj2a348
 ");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
